6db1794875239558de958168e31806cfd62fab1e
germaan07 <ortep09@gmail.com>
2026-02-12 14:29:04 +0100

    SeccondCommit

--- src/main/java/org/example/ej5/Pant2.java
+++ src/main/java/org/example/ej5/Pant2.java
@@ -7,48 +7,44 @@ public class Pant2 {
         Scanner sc = new Scanner(System.in);
 
         boolean cont = true;
-        do {
+        while (cont){
             int m = sc.nextInt();
             int n = sc.nextInt();
 
             if (m == 0 && n == 0){
-                cont = false;
-            } else if (m % 8 == 0 && n % 8 == 0 && m < 48 && n < 48) {
-                boolean moreTwoLetters = false;
-                char[][] pantalla = new char[n][m];
-                for (int fila = 0; fila < n; fila++) {
-                    for (int columna = 0; columna < m; columna++) {
-                        System.out.print("Letra: ");
-                        char letra = sc.next().charAt(0);
-                        pantalla[fila][columna] = letra;
-                    }
+               break;
+            }
+            boolean moreTwoLetters = false;
+            char[][] pantalla = new char[n][m];
+            for (int fila = 0; fila < n; fila++) {
+                String linea = sc.next();
+                for (int columna = 0; columna < m; columna++) {
+                    char letra = linea.charAt(columna);
+                    pantalla[fila][columna] = letra;
                 }
-
-                int columDe8 = m/8;
-                int filasDe8 = n/8;
+            }
+            if (m % 8 == 0 && n % 8 == 0 && m < 48 && n < 48) {
+                int columDe8 = m / 8;
+                int filasDe8 = n / 8;
 
                 int numFila = 0;
                 int numColumna = 0;
 
                 for (int i = 0; i < filasDe8; i++) {
-                    for (int j = 0; j < columDe8; j++){
+                    for (int j = 0; j < columDe8; j++) {
                         numColumna = 8 * j;
                         numFila = 8 * i;
                         char letra1 = pantalla[numFila][numColumna];
                         char letra2 = 0;
-                        for (int fila = numFila; fila < 8 + numFila; fila++) {
-                            for (int columna = numColumna; columna < 8 + numColumna; columna++) {
+                        for (int fila = numFila; fila < 8 + numFila && !moreTwoLetters; fila++) {
+                            for (int columna = numColumna; columna < 8 + numColumna && !moreTwoLetters; columna++) {
                                 char imagen = pantalla[fila][columna];
-                                if (imagen!= letra1){
-                                    if (letra2 == 0){
+                                if (imagen != letra1) {
+                                    if (letra2 == 0) {
                                         letra2 = imagen;
-                                    }else {
-                                        if (imagen != letra2){
+                                    } else {
+                                        if (imagen != letra2) {
                                             moreTwoLetters = true;
-                                            System.out.println("NO");
-                                            cont = false;
-                                            j = columDe8;
-                                            i = filasDe8;
                                         }
                                     }
                                 }
@@ -58,11 +54,12 @@ public class Pant2 {
                 }
                 if (!moreTwoLetters){
                     System.out.println("SI");
+                } else {
+                    System.out.println("NO");
                 }
-            } else {
-                cont = true;
-                System.out.println("Error");
+            }else{
+                System.out.println("NO");
             }
-        } while (cont);
+        }
     }
 }
